c1365492d8167748edfd124563f1315b89071c6d
Mojtaba-Gholizadeh <mojtaba.leoooo@gmail.com>
2025-01-25 00:07:49 +0330

    ecommerce project created and configured

---
+++ lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'AvidStore'
+export const APP_DESCRIPTION =
+    process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A modern ecommerce paltform build with Next.js'
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
